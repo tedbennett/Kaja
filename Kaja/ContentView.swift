312a892c1418da60edb35a3c66fd9ef704cbd0ac
Ted <teddsbennett@gmail.com>
2020-07-11 11:38:52 +0100

    Initial Commit

---
+++ Kaja/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Kaja
+//
+//  Created by Ted Bennett on 11/07/2020.
+//  Copyright © 2020 Ted Bennett. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
